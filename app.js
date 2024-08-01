7633d847218bdeef05c5391c1e255e2f7188f21c
ClaytonDewey <clay@dryadmedia.com>
2024-08-01 15:32:55 -0500

    Add home route

--- app.js
+++ app.js
@@ -31,6 +31,9 @@ app.use(helmet());
 app.use(cors());
 app.use(xss());
 // routes
+app.get('/', (req, res) => {
+  res.send('<h1>Jobs API</h1><a href="/api-docs">Documentation</a>');
+});
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/jobs', authenticateUser, jobsRouter);
 
